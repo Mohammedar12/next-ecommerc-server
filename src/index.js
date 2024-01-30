783027cc1471106ce30a21cda9f61aebe098ed4d
Mohammedar12 <alikkar87@gmail.com>
2024-01-30 20:37:03 +0300

    add cors

--- src/index.js
+++ src/index.js
@@ -38,12 +38,9 @@ app.use(
   cookieSession({
     name: "session",
     keys: ["secretKey1"],
-    maxAge: 24 * 60 * 60 * 1000,
-    domain:
-      process.env.NODE_ENV === "production" ? ".netlify.app" : "localhost",
-    // sameSite: "None", // Set to 'None' for cross-site requests
-    secure: process.env.NODE_ENV === "production", // Set to true in production
-    httpOnly: true, // Set to false if JavaScript needs to access the cookie
+    maxAge: 24 * 60 * 60 * 1000, // 24 hours in milliseconds
+    secure: true, // Set to true since your application is served over HTTPS
+    domain: "mynextappstore.netlify.app", // Set to your Netlify domain
   })
 );
 
